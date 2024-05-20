66988749d8bdded55db2ca9bff5ee566b77598eb
Adam Dusty <adam.dusty27@gmail.com>
2024-05-20 15:23:06 -0700

    Update fetch content dependencies

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -4,7 +4,6 @@ FetchContent_Declare(
     yaml-cpp
     GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
     GIT_TAG        76dc6715734295ff1866bfc32872ff2278258fc8 # master as of 4/16/2024
-    GIT_SHALLOW    TRUE
     SYSTEM
 )
 
@@ -12,7 +11,6 @@ FetchContent_Declare(
     argparse
     GIT_REPOSITORY https://github.com/p-ranav/argparse.git
     GIT_TAG        ac4c2c2d96c974c6a4e487f288bfc89e108b64b1 # master as of 4/29/2024
-    GIT_SHALLOW    TRUE
     SYSTEM
 )
 
